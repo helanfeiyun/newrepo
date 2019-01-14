98044c8e3b3ea9608fa9d94fcd97d1f98e3f4236
HeYiheng <369980587@qq.com>
2019-01-14 13:55:37 +0800

    修改hello.php文件

--- hello.php
+++ hello.php
@@ -1,4 +1,5 @@
 <? php
 echo "Yiheng 用 vim 修改的 hello.php 文件";
-echo "I am trying to typing something else (- . -)! "
+echo "I am trying to typing something else (- . -)! ";
+echo "git reset HEAD , I want to see what will change."
 ?>
